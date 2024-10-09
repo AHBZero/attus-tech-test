35ccb0e14a8412471eb1071c2a486a6744da03d1
Bernardi Alex <alexbernardi2010@hotmail.com>
2024-10-09 01:32:32 -0300

    spring+docker

---
+++ src/main/java/com/techtest/attus_tech_test/AttusTechTestApplication.java
@@ -0,0 +1,13 @@
+package com.techtest.attus_tech_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttusTechTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttusTechTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtest/attus_tech_test/AttusTechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtest.attus_tech_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttusTechTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
